0276c1152e4f5b2d65a7cc2a301c34233985f4b4
harshj-1703 <harshj6680@gmail.com>
2023-03-27 14:27:54 +0530

    update

--- test_route/index.js
+++ test_route/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000;
-app.get('/',(req,resp)=>{
-    resp.send("welcome to get function")
-});
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
+});
+app.route('/').get((req,resp)=>{
+    resp.send("welcome to get function")
+}).post((req,resp)=>{
+    resp.send("welcome to post function")
+}).put((req,resp)=>{
+    resp.send("welcome to put function")
+}).delete((req,resp) => {
+    resp.send("welcome to delete function")
 });

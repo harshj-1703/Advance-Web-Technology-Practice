3bcf2298be81cff4ceb65129941ce51cef8d73db
harshj-1703 <harshj6680@gmail.com>
2023-03-27 14:43:38 +0530

    update

--- test_route/index.js
+++ test_route/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000;
+
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 });
+
 app.route('/').get((req,resp)=>{
     resp.send("welcome to get function")
 }).post((req,resp)=>{
@@ -12,4 +14,17 @@ app.route('/').get((req,resp)=>{
     resp.send("welcome to put function")
 }).delete((req,resp) => {
     resp.send("welcome to delete function")
+});
+
+// app.route('*').all((req,resp,next)=>{
+//     console.log("Accessing invalid section....");
+//     resp.status(404)
+//     resp.send('invalid page')
+// });
+
+app.get('/ab?cd', (req, res) => {
+    res.send('ab?cd')
+});
+app.get('/functions?', (req, res) => {
+    res.send('function?s')
 });

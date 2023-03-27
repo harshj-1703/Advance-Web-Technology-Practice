d41c43e1ca30610ad24dbcc6f6006b54b958dbe6
harshj-1703 <harshj6680@gmail.com>
2023-03-27 14:18:31 +0530

    update

--- test_route/index.js
+++ test_route/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 5000;
-
+app.get('/',(req,resp)=>{
+    resp.send("welcome to get function")
+});
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 });

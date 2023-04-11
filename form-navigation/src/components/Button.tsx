f7538a29f683c36905020649fc9f16fb6553ad35
harshj-1703 <harshj6680@gmail.com>
2023-04-11 13:54:54 +0530

    update

---
+++ form-navigation/src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { MouseEvent } from "react";
+
+interface Props {
+  color: "primary" | "warning" | "success";
+  children: string;
+  handleClick: (e: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button = ({ color, children, handleClick }: Props) => {
+  return (
+    <button type="button" className={"btn btn-" + color} onClick={handleClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- form-navigation/src/components/Home.tsx
+++ form-navigation/src/components/Home.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { MouseEvent } from "react";
+import Button from "./Button";
 
 const Home = () => {
-  return <div>Welcome to my website</div>;
+  const handleClick = () => {
+    console.log("Button clicked from home ");
+  };
+  const handleClickWarning = (e: MouseEvent<HTMLButtonElement>) => {
+    console.log("Warning Button clicked from home ", e.target);
+  };
+  return (
+    <div>
+      Welcome to my website
+      <Button handleClick={handleClick} color="primary">
+        This is test button
+      </Button>
+      <Button handleClick={handleClickWarning} color="warning">
+        This is warning button
+      </Button>
+    </div>
+  );
 };
 
 export default Home;

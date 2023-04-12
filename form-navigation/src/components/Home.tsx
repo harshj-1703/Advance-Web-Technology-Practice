7810367535c800f1c89b64372c268a81fbab530d
harshj-1703 <harshj6680@gmail.com>
2023-04-12 12:09:40 +0530

    react-crud

--- form-navigation/src/components/Home.tsx
+++ form-navigation/src/components/Home.tsx
@@ -9,15 +9,75 @@ const Home = () => {
     console.log("Warning Button clicked from home ", e.target);
   };
   return (
-    <div>
-      Welcome to my website
+    <>
+      {/* Welcome to my website
       <Button handleClick={handleClick} color="primary">
         This is test button
       </Button>
       <Button handleClick={handleClickWarning} color="warning">
         This is warning button
-      </Button>
-    </div>
+      </Button> */}
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Title"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Description</label>
+          <textarea className="form-control" placeholder="Enter Description" />
+        </div>
+        <div className="form-group form-check"></div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <br />
+      <br />
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleClick}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="btn btn-warning"
+                onClick={handleClick}
+              >
+                Update
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={handleClick}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 };
 
